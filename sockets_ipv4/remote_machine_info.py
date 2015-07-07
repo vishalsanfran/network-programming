e36c2d33f23a4de3073fadd9adc3ffd3af33a848
vishalsanfran <vishalkk@gmail.com>
2015-07-07 21:39:48 +0000

    added packed/ unpacked ipaddr support

--- sockets_ipv4/remote_machine_info.py
+++ sockets_ipv4/remote_machine_info.py
@@ -6,6 +6,14 @@ def get_remote_machine_info():
         print "IP address: %s" %socket.gethostbyname(remote_host)
     except socket.error, err_msg:
         print "%s: %s" %(remote_host, err_msg)
+        
+def convert_ip4_address():
+    for ip_addr in ['127.0.0.1', '192.168.0.1']:
+        packed_ip_addr = socket.inet_aton(ip_addr)
+        unpacked_ip_addr = socket.inet_ntoa(packed_ip_addr)
+        print "IP Address: %s => Packed: %s, Unpacked: %s"\
+	 %(ip_addr, hexlify(packed_ip_addr), unpacked_ip_addr)
     
 if __name__ == '__main__':
+    convert_ip4_address()
     get_remote_machine_info()
